dd48609733339bbf1919649ab061aec0b9f3cbd2
jameskalfox <75384370+jameskalfox@users.noreply.github.com>
2022-12-19 17:10:06 +0000

    Minor sql changes

--- Week_1_Basic_External_Stages/Week 1 – Basic - External Stages.sql
+++ Week_1_Basic_External_Stages/Week 1 – Basic - External Stages.sql
@@ -3,7 +3,7 @@
 -- create/use warehouse
 
 -- create external stage
-create stage week_1_stage
+create or replace stage week_1_stage
     url = 's3://frostyfridaychallenges/challenge_1/';
 
 -- check files in external stage
@@ -13,7 +13,7 @@ list @week_1_stage;
 select $1, $2 from @week_1_stage;
 
 -- create table
-create table week_1_table
+create or replace table week_1_table
     (col1 varchar);
 
 -- copy into table
