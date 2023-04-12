a340ee60762b619d134964bded7155bbf1aad3da
jameskalfox <75384370+jameskalfox@users.noreply.github.com>
2023-04-12 16:05:09 +0100

    Solution added

---
+++ Week_22.sql
@@ -0,0 +1,29 @@
+// Create secure view using UUID
+CREATE OR REPLACE SECURE VIEW secure_cities AS 
+    (select 
+        UUID_STRING() as id,
+        city,
+        district
+    from week22);
+
+// Create row access policy that returns based on role and mod of id
+create or replace row access policy row_cities as (id int)
+returns boolean ->
+    is_role_in_session('sysadmin')
+    or (is_role_in_session('rep1') and id % 2 = 1)
+    or (is_role_in_session('rep2') and id % 2 = 0);
+
+// Add rap to table underneath the secure view on the id column
+alter table week22 add row access policy row_cities on (id);
+
+
+----- OPTIONAL -----
+
+// Use masking policy to mask id
+CREATE OR REPLACE MASKING POLICY id_mask AS (id int) RETURNS int ->
+  CASE
+    WHEN CURRENT_ROLE() = ('ACCOUNTADMIN') THEN id
+    ELSE random(123)
+  END;
+
+ALTER view secure_cities MODIFY COLUMN id SET MASKING POLICY id_mask;
---
+++ Week_39.sql
@@ -0,0 +1,23 @@
+// Setup code
+create or replace table customer_deets (
+    id int,
+    name string,
+    email string
+);
+
+insert into customer_deets values
+    (1, 'Jeff Jeffy', 'jeff.jeffy121@gmail.com'),
+    (2, 'Kyle Knight', 'kyleisdabest@hotmail.com'),
+    (3, 'Spring Hall', 'hall.yay@gmail.com'),
+    (4, 'Dr Holly Ray', 'drdr@yahoo.com');
+
+// Create masking policy
+create or replace masking policy email_mask as (email string) 
+returns string ->
+    CASE 
+        WHEN contains(current_role(), 'SYSADMIN') then email
+        ELSE REGEXP_REPLACE(email, '(.*)@', '****@')
+    END;
+
+// Apply masking policy
+alter table customer_deets modify column email set masking policy email_mask;
